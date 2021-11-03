6b0380c0f6faf4a8c99c0b47154dbc616b8a193d
Dants96 <danieltutistar@gmail.com>
2021-11-03 17:02:50 -0500

    actividad terminada

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { pipe } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items:any={};
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get("https://www.datos.gov.co/resource/tyhr-3h8y.json")
+    .subscribe(
+      result => {                      
+        this.items = result;
+      },
+      error => {
+        console.log('No se pudo asignar el item ');
+      }
+    );
+  }
+}
